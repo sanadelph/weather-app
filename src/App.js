c999ba8bee1ae4f1e155acccd10c7da281b6443c
Sana <s.delfieh@gmail.com>
2024-10-17 03:35:19 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useState } from "react";
 function App() {
+  const [data, setData] = useState({})
+  const [error,setError]=useState(null)
+  const [location, setLocation] = useState('')
+  const api_url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+
+  const firstLetterUpperCase=(str)=>{
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  const searchLocation=async(e)=>{
+    try {
+      if(e.key==='Enter'){
+        const res=await axios.get(api_url)
+        setData(res.data);
+        setLocation('')
+        setError(null)
+      }
+    } catch (err) {
+      setError(err.response.data.message);
+      setLocation('')
+    }
+} 
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input type="text" 
+        value={location} 
+        onChange={e=>setLocation(firstLetterUpperCase(e.target.value))}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"/>
+      </div>
+      <div className='container'>
+        {error && <p>{error}</p>}
+        <div className='top'>
+          <div className='location'>
+           {!error && <p>{data.name}</p>}
+          </div>
+          <div className='temp'>
+            {!error && data.main && <h1>{data.main?.temp.toFixed()}°F</h1>}
+          </div>
+          <div className='desc'>
+            {!error && data.weather?<p>{data.weather[0].main}</p>:null}
+          </div>
+        </div>
+
+        {!error && data.name!== undefined && 
+        
+        <div className='bottom'>
+          <div className='feels'>
+            <p className='bold'>{data.main?.feels_like.toFixed()}</p>
+            <p>Feels Like</p>
+          </div>
+          <div className='humidity'>
+            {data.main?<p className='bold'>{data.main.humidity}%</p>:null}
+            <p>Humidity</p>
+          </div>
+          <div className='wind'>
+           {data.wind? <p className='bold'>{data.wind.speed.toFixed()} MPH</p>:null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+        }
+      </div>
     </div>
   );
 }
